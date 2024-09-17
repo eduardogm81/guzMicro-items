01c96005bbfe1648024de9ab721bf175e8a8bcc4
egomez <egomez@nundusoft.com>
2024-09-17 12:47:29 +0200

    Configuración para conectar con el Gateway Server

--- src/main/java/com/micros/items/controllers/ItemController.java
+++ src/main/java/com/micros/items/controllers/ItemController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/items")
+// @RequestMapping("/api/items")
 // @RequiredArgsConstructor
 public class ItemController {
 
